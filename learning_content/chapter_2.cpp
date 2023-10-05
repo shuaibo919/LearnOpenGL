2b901ba4c11f1113c5e292b5eb51404954b35340
18658575919 <hsb919@126.com>
2023-10-05 16:59:36 +0800

    seperating chapter content

--- learning_content/chapter_2.cpp
+++ learning_content/chapter_2.cpp
@@ -1,4 +1,4 @@
-﻿// std-lib
+// std-lib
 #include <iostream>
 #include <cmath>
 // gl-lib
@@ -24,22 +24,13 @@ void mouse_callback(GLFWwindow *window, double xposIn, double yposIn);
 const unsigned int SRC_WIDTH = 800;
 const unsigned int SRC_HEIGHT = 600;
 
-// camera
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
 bool firstMouse = true;
-float yaw = -90.0f; // yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
-float pitch = 0.0f;
-float lastX = 800.0f / 2.0;
-float lastY = 600.0 / 2.0;
-float fov = 45.0f;
 
 // timing
 float deltaTime = 0.0f; // time between current frame and last frame
 float lastFrame = 0.0f;
-
+float lastX = (float)SRC_WIDTH / 2.0;
+float lastY = (float)SRC_HEIGHT / 2.0;
 // camera
 GLBasicCamera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
@@ -73,27 +64,6 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    // cube类
-    GLTextureCube cube = GLTextureCube();
-    // 加载并绑定纹理
-    GLuint woodTexture = loadTexture("resource/img/container.jpg", GL_RGB);
-    GLuint faceTexture = loadTexture("resource/img/texture.jpg", GL_RGB);
-    auto textures = std::vector<GLuint>({woodTexture, faceTexture});
-    cube.setUniform("texture1", 0);
-    cube.setUniform("texture2", 1);
-    // 生成十个不同的cube对象
-    glEnable(GL_DEPTH_TEST);
-    glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(2.0f, 5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f, 3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f, 2.0f, -2.5f),
-        glm::vec3(1.5f, 0.2f, -1.5f),
-        glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     // Render Loop
     while (!glfwWindowShouldClose(window))
@@ -101,27 +71,11 @@ int main()
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-
         processInput(window);
-        // render 176,168,185
-        glClearColor(0.69f, 0.66f, 0.72f, 1.0f);
+        glClearColor(0.f, 0.f, 0.f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         // view
-        glm::mat4 view = camera.viewMatrix();
-        glm::mat4 projection = camera.projectionMatrix(SRC_WIDTH, SRC_HEIGHT);
-        cube.setUniform("projection", projection);
-        cube.setUniform("view", view);
-        for (unsigned int i = 0; i < 10; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, angle + (float)glfwGetTime() * glm::radians(50.0f) * (i != 0), glm::vec3(1.0f, 0.3f, 0.5f));
-            // 摄像机
-            cube.setUniform("model", model);
-            cube.draw(textures);
-        }
+
         // swap cache
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -175,30 +129,4 @@ void mouse_callback(GLFWwindow *window, double xposIn, double yposIn)
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
 {
     camera.processMouseScroll(static_cast<float>(yoffset));
-}
-GLuint loadTexture(const char *file_path, GLuint internalFormat)
-{
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    // 为当前绑定的纹理对象设置环绕、过滤方式
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // 加载并生成纹理
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load(file_path, &width, &height, &nrChannels, 0);
-    stbi_set_flip_vertically_on_load(true);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, internalFormat, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    return texture;
 }
